149c646349905f87016f826cb2747b5d942b6740
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2022-01-29 18:17:12 +0200

    Create 270A.cpp

---
+++ 270A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define F first
+#define S second
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define rep(i, a, n) for(size_t i = a; i < n; i++)
+#define sz(str) str.size()
+#define zeros(x) memset(x,0,sizeof(x))
+
+void IO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int GCD(int n, int k){
+    if(k == 0)  
+        return n;
+    return (k, n % k);
+}
+
+int LCM(int n, int k){
+    return (n * k)/GCD(n, k);
+}
+
+int main(){
+    int TC, x;
+    scanf("%d", &TC);
+    while(TC--){
+        scanf("%d", &x);
+        double temp = (360.0 / (180.0 - x));
+        if(temp - floor(temp) == 0)
+            printf("YES\n");
+        else    
+            printf("NO\n");
+    }
+}
